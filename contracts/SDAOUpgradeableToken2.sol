5c4ecd4c2de3f78cdff4b5c48daffa262f4e136d
Sridhar Babu Kolapalli <ksridharbabuus@gmail.com>
2021-10-22 19:33:49 +0530

    Updated code and test cases

--- contracts/SDAOUpgradeableToken2.sol
+++ contracts/SDAOUpgradeableToken2.sol
@@ -6,13 +6,21 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract SDAOUpgradableToken is Initializable, OwnableUpgradeable, ERC20Upgradeable {
+contract SDAOUpgradeableToken2 is Initializable, OwnableUpgradeable, ERC20Upgradeable {
     
+    bool v2upgrade;
 
     function initialize(string memory name, string memory symbol, uint256 initialSupply) public virtual initializer {
         __ERC20_init(name, symbol);
         __Ownable_init();
         _mint(_msgSender(), initialSupply);
+    }
+
+    function additionalMint(uint256 newSupply) public onlyOwner {
+
+     	require (!v2upgrade,"already upgraded");
+        _mint(_msgSender(), newSupply);
+        v2upgrade = true;
     }
 
 }
